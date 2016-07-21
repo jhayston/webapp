72e22100cd648af1487fa9acbce4e97be44e76d6
jhayston <jhayston@gordianinnovation.com>
2016-07-21 16:22:44 -0400

    Initial commit for our first spring boot project

---
+++ src/main/java/com/gordianinnovation/GordianAdminAppApplication.java
@@ -0,0 +1,12 @@
+package com.gordianinnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GordianAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GordianAdminAppApplication.class, args);
+	}
+}
